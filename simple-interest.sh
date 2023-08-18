367c68bd68bb42eef6177132764217b14563df12
Mansi Kamble <113781741+MansiRK@users.noreply.github.com>
2023-08-18 23:45:52 +0530

    Update simple-interest.c

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,26 +1,30 @@
-   #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
-   # Do not use this in production. Sample purpose only.
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # MansiRK
    # Input:
    # p, principal amount
    # t, time period in years
    # r, annual rate of interest
    # Output:
    # simple interest = p*t*r
-   
-   echo "Enter the principal:"
-   read p
-   
-   echo "Enter rate of interest per year:"
-   read r
-   
-   echo "Enter time period in years:"
-   read t
-   
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
+
+
+# include <stdio.h>
+
+int main() {
+
+   int p, r, t;
+   float s;
+
+   printf("Enter the principal:");
+   scanf("%d", &p);
+
+   printf("Enter rate of interest per year:");
+   scanf("%d", &r);
+
+   printf("Enter time period in years:");
+   scanf("%d", &t);
+
+   s = (p * t * r)/100;
+
+   printf("The simple interest is: ", s);
+
+}
+  
